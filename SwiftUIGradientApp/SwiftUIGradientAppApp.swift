b76b4b4855b7052a3d073dc103efcc8d98fa5416
Konstantin Prokofiev <flashboyz@yandex.ru>
2021-09-12 16:12:04 +0300

    Initial Commit

---
+++ SwiftUIGradientApp/SwiftUIGradientAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGradientAppApp.swift
+//  SwiftUIGradientApp
+//
+//  Created by Константин Прокофьев on 12.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGradientAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
